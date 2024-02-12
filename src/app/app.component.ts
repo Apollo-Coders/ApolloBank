f4f0022344f1c529c81a2260fa46dffbaf40251b
caiohxp <caiocesar.ccomp@gmail.com>
2024-02-12 08:49:19 -0300

    first minha-conta commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MinhaContaComponent } from './minha-conta/minha-conta.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MinhaContaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MinhaContaComponent } from './minha-conta/minha-conta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'minha-conta', pathMatch: 'full' },
+    { path: 'minha-conta', component: MinhaContaComponent },
+    { path: '**', redirectTo: 'minha-conta' }
+];
