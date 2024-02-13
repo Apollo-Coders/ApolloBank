0b743b2d4247a16f17edec941ca5718f303e4cbf
Daiane Bolzan <dainha85@gmail.com>
2024-02-12 22:02:11 -0300

    feat: update register page structure

--- src/app/models/register-page/register-page.component.ts
+++ src/app/models/register-page/register-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RegisterFormComponent } from '../../components/register-form/register-form.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [RegisterFormComponent],
+  imports: [RegisterFormComponent, NavbarComponent],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
