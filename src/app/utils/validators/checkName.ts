23593e0d6ce215decb62ec345989072a04a32070
Daiane Bolzan <dainha85@gmail.com>
2024-02-13 19:07:40 -0300

    feat: add validators to birthday, cpf, name and phone

--- src/app/utils/validators/checkName.ts
+++ src/app/utils/validators/checkName.ts
@@ -4,12 +4,18 @@ export function nameValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const value = (control.value || '').trim();
     const words = value.split(' ').filter(Boolean);
+
+    const validCharsRegex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ ]+$/;
+
     if (words.length < 2) {
       return { 'nameInvalid': true, 'reason': 'Not enough words' };
     }
     for (let word of words) {
       if (word.length < 3) {
         return { 'nameInvalid': true, 'reason': 'Word too short' };
+      }
+      if (!validCharsRegex.test(word)) {
+        return { 'nameInvalid': true, 'reason': 'Invalid characters' };
       }
     }
 
