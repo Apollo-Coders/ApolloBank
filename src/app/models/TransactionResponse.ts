5e23ee3edeaf1dce183cbf731bb931988ba6e5b6
Guilherme Duarte <guilhduart.abr@gmail.com>
2024-02-13 12:45:36 -0300

    DTOs Models

---
+++ src/app/models/TransactionResponse.ts
@@ -0,0 +1,34 @@
+export enum TransactionType {
+  DEPOSIT = 'Deposito',
+  WITHDRAW = 'Saque',
+  TRANSFER_TO = 'Transfência',
+  TRANSFER_FROM = 'Transfência',
+}
+
+export class Transaction {
+  amount: number;
+  to: string | null;
+  from: string | null;
+  date: Date;
+  description: string;
+  transactionType: TransactionType;
+  direction: '+' | '-';
+
+  constructor(
+    amount: number,
+    direction: '+' | '-',
+    to: string | null,
+    from: string | null,
+    date: Date,
+    description: string,
+    transactionType: TransactionType
+  ) {
+    this.amount = amount;
+    this.direction = direction;
+    this.to = to;
+    this.from = from;
+    this.date = date;
+    this.description = description;
+    this.transactionType = transactionType;
+  }
+}
