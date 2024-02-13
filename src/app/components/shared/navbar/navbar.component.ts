e26e7b44cb2d892b2de76246a4a0be7cbae11cef
Daiane Bolzan <dainha85@gmail.com>
2024-02-13 12:34:24 -0300

    feat: add collapsed menu to navbar

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,12 +1,15 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgbCollapseModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  isCollapsed = true;
 }
