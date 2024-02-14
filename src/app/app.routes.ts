b952dc716e1f409de48bf60ec47e9ce45fc1c8e6
Daiane Bolzan <dainha85@gmail.com>
2024-02-13 21:04:32 -0300

    feat: add some routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'home',
+        component: HomeComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./components/register-page/register-page.component').then(m => m.RegisterPageComponent)
+    },
+
+    {
+        path: 'not-found',
+        loadComponent: () => import('./components/not-found/not-found.component').then(m => m.NotFoundComponent)
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'not-found'
+    }
+];
