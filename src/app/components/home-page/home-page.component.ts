c0469e439a6a8c33b62ad98e33de5a8ee5bdde21
Carla Batista <batista.eduardacomex@gmail.com>
2024-02-14 14:18:56 -0300

    Entrega da HomePage

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [ NavbarComponent, FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
