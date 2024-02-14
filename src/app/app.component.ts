24106f319010f02798169f9670f863c62680958f
Lucas Vinicius <lucas.vinicius03@outlook.com>
2024-02-14 01:59:52 -0300

    feature: added imports for created component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { SubsequentRegistrationPageComponent } from './components/subsequent-registration-page/subsequent-registration-page.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RegisterPageComponent],
+  imports: [CommonModule, RouterOutlet, RegisterPageComponent, SubsequentRegistrationPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
