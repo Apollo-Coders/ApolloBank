ec09c2166df92348feb44c3eacb618715da1d994
Daiane Bolzan <dainha85@gmail.com>
2024-02-14 17:14:09 -0300

    feat: add register success page

---
+++ src/app/components/shared/register-sucess/register-sucess.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register-sucess',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './register-sucess.component.html',
+  styleUrl: './register-sucess.component.css'
+})
+export class RegisterSucessComponent {
+
+}
