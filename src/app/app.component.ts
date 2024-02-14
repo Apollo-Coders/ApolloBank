855eecb302a95438121b248b1e9e2a2132b05bb9
Daiane Bolzan <dainha85@gmail.com>
2024-02-14 17:20:04 -0300

    feat: remove unnecessary imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { RegisterPageComponent } from './components/register-page/register-page.component';
-import { HomeComponent } from "./components/home/home.component";
-import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, RegisterPageComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet]
 })
 export class AppComponent {
   title = 'ApolloBank';
