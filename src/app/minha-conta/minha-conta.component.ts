3e3395840535cbfa33e7d042bda467fd880292cc
caiohxp <caiocesar.ccomp@gmail.com>
2024-02-16 06:07:30 -0300

    change view and icon transfer

--- src/app/minha-conta/minha-conta.component.ts
+++ src/app/minha-conta/minha-conta.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-minha-conta',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './minha-conta.component.html',
   styleUrl: './minha-conta.component.css'
 })
 export class MinhaContaComponent {
   user = 'Usuário';
+  transacoes = [{
+    tipo: "down",
+    nome: "Compra no mercado",
+    valor: "50.00"
+  },
+  {
+    tipo: "down",
+    nome: "Transferência para João",
+    valor: "200.00"
+  },
+  {
+    tipo: "up",
+    nome: "Depósito em dinheiro",
+    valor: "100.00"
+  }
+  ];
+
+  changeViewSaldo() {
+    var saldo = document.querySelector('.saldo input');
+    var btnchange = document.querySelector('.saldo .bi');
+    console.log(btnchange);
+
+    var type = saldo?.getAttribute("type");
+    if (type == "number") {
+      type = "password";
+      btnchange?.classList.replace("bi-eye-slash", "bi-eye");
+    } else {
+      type = "number";
+      btnchange?.classList.replace("bi-eye", "bi-eye-slash");
+    }
+    saldo?.setAttribute("type", `${type}`);
+
+  }
 }
