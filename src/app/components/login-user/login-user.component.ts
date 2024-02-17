360a609ab75278082a87d610156b6c47771d131e
Brenda Aniceto Bueno <brendabuenobrito@gmail.com>
2024-02-16 22:45:53 -0300

    Implementado o outpute entre os componentes

--- src/app/components/login-user/login-user.component.ts
+++ src/app/components/login-user/login-user.component.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { LoginFormComponent } from '../shared/login-form/login-form.component';
+import { CommonModule } from '@angular/common';
+import { PasswordFormComponent } from '../shared/password-form/password-form.component';
 
 
 
 @Component({
   selector: 'app-login-user',
   standalone: true,
-  imports: [NavbarComponent, LoginFormComponent ],
+  imports: [NavbarComponent, LoginFormComponent, CommonModule, PasswordFormComponent ],
   templateUrl: './login-user.component.html',
   styleUrl: './login-user.component.css'
 })
 export class LoginUserComponent {
   navbarButton = 'Abra sua conta';
-  route="/register"
+  route="/register";
+  currentForm = 'register';
+
+  FormLogin:boolean= true;
+  FormPassword:boolean= false; 
+
+  switchToSubsequentForm() {
+   this.FormLogin = false;
+   this.FormPassword = true;
+  }
+
+
+
 }
--- src/app/components/shared/login-form/login-form.component.ts
+++ src/app/components/shared/login-form/login-form.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
+import { CommonModule, FormStyle } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Form, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [NgbCollapseModule],
+  imports: [NgbCollapseModule,ReactiveFormsModule, CommonModule ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
+
+
+
 export class LoginFormComponent {
   isCollapsed = true;
+  
+  constructor(private fb: FormBuilder) { }
+  form!: FormGroup;
+  errorMessage: string | null = '';
+  private trimFormValues(formValues: string): string {
+    const trimmedValues: any = {};
+
+    (Object.keys(formValues) as Array<keyof string>).forEach(key => {
+      const value = formValues[key];
+      if (typeof value === 'string') {
+        trimmedValues[key] = value.trim();
+      } else {
+
+        trimmedValues[key] = value;
+      }
+    });
+
+    return trimmedValues as string;
+  }
+
+
+
+  @Output() formCompleted = new EventEmitter<void>();
+
+  onSubmit() {
+    if (this.form.valid) {
+      const formValues: string = this.form.value;
+      const cleanValues = this.trimFormValues(formValues);
+      console.log('Form Data: ', cleanValues);
+      this.form.reset();
+      this.formCompleted.emit();
+    }
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      cpf: new FormControl('', [
+        Validators.required,
+        Validators.minLength(11),
+        Validators.maxLength(11),
+        Validators.pattern("^[0-9]*$") // Esta expressão regular permite apenas números
+      ])
+    });
+  }
+
 }
