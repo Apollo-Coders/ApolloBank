779ced876ab94223319e469a04675f04a70dae16
Daiane Bolzan <dainha85@gmail.com>
2024-02-17 13:21:11 -0300

    feat: update routes and remove unnecessary component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 
 export const routes: Routes = [
@@ -10,7 +9,6 @@ export const routes: Routes = [
         path: 'register',
         loadComponent: () => import('./components/register-page/register-page.component').then(m => m.RegisterPageComponent)
     },
-
     {
         path: 'not-found',
         loadComponent: () => import('./components/not-found/not-found.component').then(m => m.NotFoundComponent)
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ NavbarComponent, FooterComponent],
+  imports: [ NavbarComponent, FooterComponent, RouterLink],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
-
+  route = "/login";
 }
