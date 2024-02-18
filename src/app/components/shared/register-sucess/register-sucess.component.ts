c2c12c46febba8f40889f18efeefc8767361449a
Daiane Bolzan <dainha85@gmail.com>
2024-02-17 21:16:09 -0300

    fix: adjust size

--- src/app/components/shared/register-sucess/register-sucess.component.ts
+++ src/app/components/shared/register-sucess/register-sucess.component.ts
@@ -11,4 +11,5 @@ import { RouterLink } from '@angular/router';
 export class RegisterSucessComponent {
 title: string = 'Cadastro criado!';
 paragraph: string = 'Faça login na sua conta e desfrute de todos os benefícios e liberdade que o banco Apollo pode te fornecer!';
+buttonText: string = 'Acesse sua conta';
 }
