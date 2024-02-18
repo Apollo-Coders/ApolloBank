df52bf09e4ef4b998b301d6fc9917530cfc3765b
luizaferreirafonseca <luiza.ferreira.fonseca@gmail.com>
2024-02-18 12:26:21 -0300

    adding sections

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { SectionsComponent } from '../../sections/sections/sections.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ NavbarComponent, FooterComponent],
+  imports: [ NavbarComponent, FooterComponent, SectionsComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
