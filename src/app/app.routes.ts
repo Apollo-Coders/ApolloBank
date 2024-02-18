44a049e966b5a61c3bdeb3d9c234f1092015ad49
Brenda Aniceto Bueno <brendabuenobrito@gmail.com>
2024-02-18 12:57:39 -0300

    Removendo componente de rotas do password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LoginUserComponent } from './components/login-user/login-user.component';
-import { LoginPasswordComponent } from './components/login-password/login-password.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 
