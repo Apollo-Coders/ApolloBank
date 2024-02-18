335be53c71e88745e33b1274ffb6c918c38af148
Daiane Bolzan <dainha85@gmail.com>
2024-02-18 03:27:56 -0300

    feat: add tests to register-page

--- src/app/components/register-page/register-page.component.spec.ts
+++ src/app/components/register-page/register-page.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterPageComponent } from './register-page.component';
 import { ActivatedRoute } from '@angular/router';
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { By } from '@angular/platform-browser';
 
 describe('RegisterPageComponent', () => {
   let component: RegisterPageComponent;
@@ -17,7 +19,7 @@ describe('RegisterPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterPageComponent],
+      imports: [RegisterPageComponent, NavbarComponent],
       providers: [
         { provide: ActivatedRoute, useValue: activatedRouteStub }
       ]
@@ -26,10 +28,44 @@ describe('RegisterPageComponent', () => {
 
     fixture = TestBed.createComponent(RegisterPageComponent);
     component = fixture.componentInstance;
+    component.navbarButton = 'Test Button';
+    component.route = '/test-route';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should switch to subsequent form', () => {
+    component.switchToSubsequentForm();
+    expect(component.currentForm).toBe('subsequent');
+  });
+  it('should switch to succes form', () => {
+    component.switchToSuccess();
+    expect(component.currentForm).toBe('success');
+  });
+  it('should switch to register form', () => {
+    component.switchToFirst();
+    expect(component.currentForm).toBe('register');
+  });
+
+  it('should switch back to the register form', () => {
+    component.switchToSubsequentForm(); 
+    component.switchToFirst(); 
+    expect(component.currentForm).toBe('register');
+  });
+
+
+  it('should pass navbarButton and route to app-navbar', () => {
+    const navbarComponent: NavbarComponent = fixture.debugElement.query(By.directive(NavbarComponent)).componentInstance;
+    expect(navbarComponent.navbarButton).toEqual('Test Button');
+    expect(navbarComponent.navButtonRoute).toEqual('/test-route');
+  });
+
+  it('should start with the register form', () => {
+    expect(component.currentForm).toBe('register');
+  });
+  
+
 });
