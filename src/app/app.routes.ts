a1e85bc2215248ba7bb84fd85c0505016151e363
Guilherme Duarte <guilhduart.abr@gmail.com>
2024-02-19 15:30:15 -0300

    feat: change folder and adding credit info

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,38 @@
 import { Routes } from '@angular/router';
 import { LoginUserComponent } from './components/login-user/login-user.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { MinhaContaComponent } from './minha-conta/minha-conta.component';
+import { MinhaContaComponent } from './components/minha-conta/minha-conta.component';
 import { TransactionHistoryPageComponent } from './components/transaction-history-page/transaction-history-page.component';
 
-
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomePageComponent },
-    { path: 'login', component: LoginUserComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'login', component: LoginUserComponent },
 
-    {
-        path: 'register',
-        loadComponent: () => import('./components/register-page/register-page.component').then(m => m.RegisterPageComponent)
-    },
-    {
-        path: 'not-found',
-        loadComponent: () => import('./components/not-found/not-found.component').then(m => m.NotFoundComponent)
-    },
-    {
-        path: 'minha-conta', component: MinhaContaComponent
-    },
-    {
-        path: 'transactions', component: TransactionHistoryPageComponent
-    },
-    {
-        path: '**',
-        redirectTo: 'not-found'
-    }
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./components/register-page/register-page.component').then(
+        (m) => m.RegisterPageComponent
+      ),
+  },
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./components/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
+  },
+  {
+    path: 'minha-conta',
+    component: MinhaContaComponent,
+  },
+  {
+    path: 'transactions',
+    component: TransactionHistoryPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+  },
 ];
-
-
