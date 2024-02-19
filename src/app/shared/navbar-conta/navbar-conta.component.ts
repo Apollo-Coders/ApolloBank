0f0cef35bad6854623b06f487b4a0ed7611e3b52
luizaferreirafonseca <luiza.ferreira.fonseca@gmail.com>
2024-02-18 23:15:15 -0300

    adding routerlink - minha-conta

--- src/app/shared/navbar-conta/navbar-conta.component.ts
+++ src/app/shared/navbar-conta/navbar-conta.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-conta',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar-conta.component.html',
   styleUrl: './navbar-conta.component.css'
 })
