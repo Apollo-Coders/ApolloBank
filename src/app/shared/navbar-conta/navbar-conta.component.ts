8f4956f282e87b94dcbe68b931d24bea066f64a9
Guilherme Duarte <guilhduart.abr@gmail.com>
2024-02-19 15:29:27 -0300

    fix: screen bugs and get user name

--- src/app/shared/navbar-conta/navbar-conta.component.ts
+++ src/app/shared/navbar-conta/navbar-conta.component.ts
@@ -1,44 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {
+  ILoggedUser,
+  LocalStorageService,
+} from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-navbar-conta',
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './navbar-conta.component.html',
-  styleUrl: './navbar-conta.component.css'
+  styleUrl: './navbar-conta.component.css',
 })
-export class NavbarContaComponent {
+export class NavbarContaComponent implements OnInit {
   saldo: number = 100;
-  user = 'Usuário';
+  user!: ILoggedUser;
 
-  
+  constructor(private localstorageService: LocalStorageService) {}
+
+  ngOnInit(): void {
+    this.user = this.localstorageService.getLoggedUser();
+  }
+
   changeViewSaldo() {
     var saldo = document.querySelectorAll('.valor');
     var btnchange = document.querySelector('.saldo .bi');
 
-    var type = saldo[0]?.getAttribute("type");
+    var type = saldo[0]?.getAttribute('type');
     //verifica se o tipo do input é number e troca pra password caso seja, trocando também o icone
-    if (type == "number") {
-      type = "password";
-      btnchange?.classList.replace("bi-eye-slash", "bi-eye");
+    if (type == 'number') {
+      type = 'password';
+      btnchange?.classList.replace('bi-eye-slash', 'bi-eye');
     } else {
-      type = "number";
-      btnchange?.classList.replace("bi-eye", "bi-eye-slash");
+      type = 'number';
+      btnchange?.classList.replace('bi-eye', 'bi-eye-slash');
     }
-    saldo.forEach(s => s.setAttribute("type", `${type}`));
-
+    saldo.forEach((s) => s.setAttribute('type', `${type}`));
   }
 
-  
   getInputWidth(value: number): number {
-    const decimalCount = (value.toString()).length;
+    const decimalCount = value.toString().length;
     const baseWidth = 15;
     // Aumentar o tamanho do campo de entrada com base no número de casas decimais
     return baseWidth * decimalCount + 60;
   }
 }
-
-
-
